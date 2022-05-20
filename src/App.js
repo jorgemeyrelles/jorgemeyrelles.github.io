6baa9e17e1422e9c9502e6e6fe5421676c8c0314
jorgemeyrelles <jotaengpuc@gmail.com>
2022-05-20 18:03:42 -0300

    fix: about me

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './pages/Home';
 import { Route, Switch } from 'react-router-dom';
-import Portifolio from './pages/Portifolio';
+// import Portifolio from './pages/Portifolio';
 import AboutMe from './pages/AboutMe';
 // import Profile from './PortfolioContent/Home/Profile';
 // import Footer from './PortfolioContent/Home/Footer/Footer';
@@ -10,7 +10,7 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component= { Home } />
-      <Route exact path="/portfolio" component= { Portifolio } />
+      {/* <Route exact path="/portfolio" component= { Portifolio } /> */}
       <Route exact path="/aboutme" component={ AboutMe } />
     </Switch>
     // <div>
